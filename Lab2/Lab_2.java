9a839192e792b37a4bb6df7f3009b2e3f04d7a40
Osimjon <99245667+Osimjon@users.noreply.github.com>
2022-03-31 20:52:27 +0700

    Create Lab_2.java

---
+++ Lab2/Lab_2.java
@@ -0,0 +1,17 @@
+package Basics.Lessons;
+import java.util.Scanner;
+
+import static java.lang.Math.pow;
+
+//ввод числа, *4, ^2
+public class res {
+public static void main(String[] args) {
+  System.out.println("Введите число");
+  Scanner scanner = new Scanner(System.in);
+  
+int a = scanner.nextInt();
+int b = (int) pow((a*4),2);
+
+System.out.println(b);
+  }
+}
