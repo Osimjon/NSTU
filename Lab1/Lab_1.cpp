48e1026dc20bd37561e87026a86ca26fd1e5b838
Osimjon <99245667+Osimjon@users.noreply.github.com>
2022-03-31 21:26:49 +0700

    Update Lab_1.cpp

--- Lab1/Lab_1.cpp
+++ Lab1/Lab_1.cpp
@@ -1,1 +1,40 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+int main(){
+    setlocale(LC_ALL, "rus");
+    fstream file;
+    file.open("data.txt");
+    string data;
+    int closeBrackets = 0;
+    int openBrackets = 0;
+    char symbol;
+
+    if (!file) {
+        cout << "Файл 'data.txt' не удалось найти.\n\n";
+    }
+    else {
+        cout << "Файл успешно открыт." << endl;
+        getline(file, data);
+        cout << data << endl;
+        for (int i = 0; i < data.length(); i++) {
+            if (data[i] == '}') {
+                closeBrackets++;  
+            }
+            if (data[i] == '{') {
+                openBrackets++;
+            }
+        }
+    }
+    if (closeBrackets != openBrackets) {
+        cout << "Нарушен порядок скобок" << endl;
+    }
+    else {
+        cout << "порядок скобок не нарушен!" << endl;
+    }
+    file.close();
+    return 0;
+}
 
