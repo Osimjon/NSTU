b81a680080c4ebf76c8a33ac306a318382e60d1d
Osimjon <99245667+Osimjon@users.noreply.github.com>
2022-04-05 00:09:35 +0700

    asm

--- Lab2/Lab_2.asm
+++ Lab2/Lab_2.asm
@@ -1,1 +1,18 @@
 
+include console.inc 	; вывод в терминал
+
+.data 			; директива где объявляем переменные
+x   dd -8		; переменная
+s_4 dd 4
+.code 			; директива кода
+Start: 			; название блока
+
+	mov eax, x 	; заносем  1 значение в eax
+	mul s_4
+	mul eax
+
+  outint eax    	; выводим произведение
+  outstr " "
+  xor eax, eax
+
+end Start 		; конец программы, Start - имя первой процедуры
