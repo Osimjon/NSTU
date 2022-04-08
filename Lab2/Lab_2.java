e75d647c1cc253fc2addde91611261b79940bc1c
Osimjon <99245667+Osimjon@users.noreply.github.com>
2022-04-09 00:01:29 +0700

    Update Lab_2.java

--- Lab2/Lab_2.java
+++ Lab2/Lab_2.java
@@ -6,7 +6,6 @@ import static java.lang.Math.pow;
 //ввод числа, *4, ^2
 public class res {
 public static void main(String[] args) {
-  System.out.println("Введите число");
   Scanner scanner = new Scanner(System.in);
   
 int a = scanner.nextInt();
