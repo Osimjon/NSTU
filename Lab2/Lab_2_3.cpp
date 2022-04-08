9ef0176c446fa9f135d68b880a8d064d69ae52b4
Osimjon <99245667+Osimjon@users.noreply.github.com>
2022-04-09 00:40:33 +0700

    Update Lab_2_3.cpp

--- Lab2/Lab_2_3.cpp
+++ Lab2/Lab_2_3.cpp
@@ -5,15 +5,12 @@ using namespace std;
 
 int Turn(int n) {
 	int m = 0;
-	while (n)
-	{
+	while (n){
 		m = m * 10 + n % 10;
 		n /= 10;
 	}
 	return m;
 }
-
-
 int main() {
 	int num = 0;
 	int a = 0;
@@ -21,7 +18,6 @@ int main() {
 	
 	cout << "Enter amount of numbers:";
 	cin >> num;
-
 	for (int i = 0; i < num; i++) {
 		cout << endl << "Enter " << i+1 << " numb:";
 		cin >> a;
@@ -32,7 +28,5 @@ int main() {
 	for (int i = 0; i < num; i++) {
 		cout << myvec[i] << endl;
 	}
-	
-
 	return 0;
 }
