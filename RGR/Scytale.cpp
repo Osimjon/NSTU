a0696465804b2ab309db4220af951c3726992783
Osimjon <99245667+Osimjon@users.noreply.github.com>
2022-04-15 20:40:21 +0700

    Update Scytale.cpp

--- RGR/Scytale.cpp
+++ RGR/Scytale.cpp
@@ -45,3 +45,48 @@ vector<int> skitl(vector<int> myvec, int a) {
     return cipher;
 }
 
+
+
+vector<int> de_skitl(vector<int> myvec, int key) {
+
+    bool flagTail = false;
+    int tail = 0;
+    int lenth = myvec.size() / key;
+    if (myvec.size() % key != 0) {
+        flagTail = true;
+        tail = myvec.size() % key;
+        lenth++;
+    }
+
+
+    vector<vector<int> > shifr(lenth, vector<int>(key, 0));
+    for (vector<int>::iterator i = myvec.begin(); i != myvec.end(); i++) {
+        for (int j = 0; j < lenth; j++) {
+            for (int k = 0; k < key; k++) {
+                shifr[k][j] = myvec.at(*i);
+                i++;
+            }
+        }
+    }
+
+
+    if (flagTail) {
+        for (int i = lenth; i>(lenth - tail); i--) {
+            shifr.at(i).pop_back();
+        }
+    }
+
+
+    vector<int> de_cipher;
+    for (int i = 0; i < de_cipher.size(); i++) {
+        for (int j = 0; j < lenth; j++) {
+            for (int k = 0; k < key; k++) {
+                de_cipher[k] = shifr[j][k];
+                i++;
+            }
+        }
+    }
+
+    return de_cipher;
+
+}
