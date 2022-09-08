90d41d1d37a359d846b222dc1723d9a6e273e7be
Osimjon <99245667+Osimjon@users.noreply.github.com>
2022-09-09 03:30:24 +0700

    Update Lab_2_3.java

--- Lab2/Lab_2_3.java
+++ Lab2/Lab_2_3.java
@@ -1,4 +1,3 @@
-
 public class Main
 {
     public static int reverse(int n1){
@@ -12,10 +11,34 @@ public class Main
     return n2;
     }
     
+    public static String removefirstChar(String str)
+    {
+        if (str == null || str.length() == 0) {
+            return str;
+        }
+        return str.substring(1);
+    }
+    
+    public static String reverseString(String str) {
+        return new StringBuilder(str).reverse().toString();
+    }
+ 
+    
 	public static void main(String[] args) {
-	    int n = 250;
-	    n = reverse(n);
-		System.out.println(n);
+	    
+	    String str = "002500";
+	    
+	    while(str.charAt(0) == '0'){
+	        str = removefirstChar(str);
+	    };
+	    
+	    str = reverseString(str);
+	    
+	    while(str.charAt(0) == '0'){
+	        str = removefirstChar(str);
+	    };
+	    
+	    System.out.println(str); 
+
 	}
 }
-
