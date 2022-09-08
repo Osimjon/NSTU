82b76829cb66e3e0ce36d61377499a4b971b40a7
Osimjon <99245667+Osimjon@users.noreply.github.com>
2022-09-09 02:48:23 +0700

    Update Lab_2_3.java

--- Lab2/Lab_2_3.java
+++ Lab2/Lab_2_3.java
@@ -1,1 +1,21 @@
 
+public class Main
+{
+    public static int reverse(int n1){
+    int n2 = 0;
+    while (n1 > 0){
+        int digit = n1 % 10;
+        n1 = n1 / 10;
+        n2 = n2 * 10;
+        n2 = n2 + digit; 
+    }
+    return n2;
+    }
+    
+	public static void main(String[] args) {
+	    int n = 250;
+	    n = reverse(n);
+		System.out.println(n);
+	}
+}
+
