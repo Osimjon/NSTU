253de9f3d658ada4de35438cd3ad51dce9f8b3b9
Osimjon <99245667+Osimjon@users.noreply.github.com>
2022-09-09 00:42:15 +0700

    Create Lab_2_3.go

---
+++ Lab2/Lab_2_3.go
@@ -0,0 +1,17 @@
+package main
+
+func reverse(n int) int {
+    new_int := 0
+    for n > 0 {
+        a := n % 10
+        new_int *= 10
+        new_int += a 
+        n /= 10
+    }
+    return new_int 
+}
+
+func main() {
+    print(reverse(123456))
+    
+}
